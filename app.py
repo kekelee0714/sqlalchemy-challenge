60da9d7d69d99f61f0ff880a66207dcd08aaf046
KEFAN LI <1350-kefan.li99@users.noreply.columbia.bootcampcontent.com>
2020-07-22 20:07:49 -0400

    New

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+Base.metadata.create_all(engine)
+Base.classes.keys()
+
+measurement= Base.classes.measurement
+station= Base.classes.station
+
